3510e21f6611a21c6afd09444efe522e11ddd4a7
user <user@lorbanery>
2015-08-05 00:14:12 +0100

    make a note of the http context

--- test_request.py
+++ test_request.py
@@ -9,6 +9,7 @@ import unittest
 import logging
 
 class TestIppRequest(unittest.TestCase):
+	printer_discovery_http_prefix = b'POST / HTTP/1.1\r\nContent-Length: 635\r\nContent-Type: application/ipp\r\nHost: localhost\r\nUser-Agent: CUPS/1.5.3\r\nExpect: 100-continue\r\n\r\n'
 	printer_discovery = b'\x01\x01@\x02\x00\x00\x00\x01\x01G\x00\x12attributes-charset\x00\x05utf-8H\x00\x1battributes-natural-language\x00\x05en-gbD\x00\x14requested-attributes\x00\x12auth-info-requiredD\x00\x00\x00\ndevice-uriD\x00\x00\x00\x12job-sheets-defaultD\x00\x00\x00\x12marker-change-timeD\x00\x00\x00\rmarker-colorsD\x00\x00\x00\x12marker-high-levelsD\x00\x00\x00\rmarker-levelsD\x00\x00\x00\x11marker-low-levelsD\x00\x00\x00\x0emarker-messageD\x00\x00\x00\x0cmarker-namesD\x00\x00\x00\x0cmarker-typesD\x00\x00\x00\x10printer-commandsD\x00\x00\x00\x10printer-defaultsD\x00\x00\x00\x0cprinter-infoD\x00\x00\x00\x19printer-is-accepting-jobsD\x00\x00\x00\x11printer-is-sharedD\x00\x00\x00\x10printer-locationD\x00\x00\x00\x16printer-make-and-modelD\x00\x00\x00\x0cprinter-nameD\x00\x00\x00\rprinter-stateD\x00\x00\x00\x19printer-state-change-timeD\x00\x00\x00\x15printer-state-reasonsD\x00\x00\x00\x0cprinter-typeD\x00\x00\x00\x15printer-uri-supportedB\x00\x14requesting-user-name\x00\x04user\x03'
 	def test_consistency(self):
 		msg = IppRequest.from_string(self.printer_discovery)
