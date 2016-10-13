e42672d56d7d487f0cacba92d02560e4676c62c7
David Batley <git@dbatley.com>
2016-10-13 02:06:01 +0100

    Use ippserver.__main__ instead of ippserver.server

---
+++ ippserver/__main__.py
@@ -0,0 +1,47 @@
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import sys
+
+from . import actions
+from .server import run_server, ThreadedTCPServer, ThreadedTCPRequestHandler
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='An IPP server')
+	parser.add_argument('-v', '--verbose', action='count', help='Add debugging')
+	parser.add_argument('--host', type=str, default='localhost', metavar='HOST', help='Address to listen on')
+	parser.add_argument('--port', type=int, required=True, metavar='PORT', help='Port to listen on')
+
+	parser_action = parser.add_subparsers(help='Actions', dest='action')
+
+	parser_save = parser_action.add_parser('save', help='Write any print jobs to disk')
+	parser_save.add_argument('directory', metavar='DIRECTORY', help='Directory to save files into')
+
+	parser_command = parser_action.add_parser('run', help='Run a command when recieving a print job')
+	parser_command.add_argument('command', nargs=argparse.REMAINDER, metavar='COMMAND', help='Command to run')
+
+	return parser.parse_args()
+
+def action_function_from_args(args):
+	if args.action == 'save':
+		return actions.save_to_directory(directory=args.save)
+	if args.action == 'run':
+		return actions.run_command(command=args.command)
+	raise RuntimeError(args)
+
+def main(args):
+	logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+	server = ThreadedTCPServer(
+		(args.host, args.port),
+		ThreadedTCPRequestHandler,
+		action_function_from_args(args))
+	run_server(server)
+
+if __name__ == "__main__":
+	main(parse_args())
--- ippserver/server.py
+++ ippserver/server.py
@@ -7,11 +7,8 @@ import socket
 import threading
 import SocketServer
 import time
-import sys
 import logging
-import argparse
 
-from . import actions
 from . import http
 from . import request
 from . import logic
@@ -68,44 +65,11 @@ def wait_until_ctrl_c():
 	except KeyboardInterrupt:
 		return
 
-def parse_args():
-	parser = argparse.ArgumentParser(description='An IPP server')
-	parser.add_argument('-v', '--verbose', action='count', help='Add debugging')
-	parser.add_argument('--host', type=str, default='localhost', metavar='HOST', help='Address to listen on')
-	parser.add_argument('--port', type=int, required=True, metavar='PORT', help='Port to listen on')
-
-	parser_action = parser.add_subparsers(help='Actions', dest='action')
-
-	parser_save = parser_action.add_parser('save', help='Write any print jobs to disk')
-	parser_save.add_argument('directory', metavar='DIRECTORY', help='Directory to save files into')
-
-	parser_command = parser_action.add_parser('run', help='Run a command when recieving a print job')
-	parser_command.add_argument('command', nargs=argparse.REMAINDER, metavar='COMMAND', help='Command to run')
-
-	return parser.parse_args()
-
-def action_function_from_args(args):
-	if args.action == 'save':
-		return actions.save_to_directory(directory=args.save)
-	if args.action == 'run':
-		return actions.run_command(command=args.command)
-	raise RuntimeError(args)
-
-def main(args):
-	logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
-
-	server = ThreadedTCPServer(
-		(args.host, args.port),
-		ThreadedTCPRequestHandler,
-		action_function_from_args(args))
+def run_server(server):
 	logging.info('Listening on %r', server.server_address)
-
 	server_thread = threading.Thread(target=server.serve_forever)
 	server_thread.daemon = True
 	server_thread.start()
 	wait_until_ctrl_c()
 	logging.info('Ready to shut down')
 	server.shutdown()
-
-if __name__ == "__main__":
-	main(parse_args())
