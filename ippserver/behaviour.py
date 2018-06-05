h2g2bob/ipp-server

commit 4003b8ba8bf9e1537c6c5c850eab351bb442670a

replace Integer(0).bytes() by b"\x00" (endianess not important if all bits are zero), use %i as python independent integer parameter

ippserver/behaviour.py
@@ -196,7 +196,7 @@ class StatelessPrinter(Behaviour):
 			(SectionEnum.printer, b'document-format-default', TagEnum.mime_media_type) : [b'application/pdf'],
 			(SectionEnum.printer, b'document-format-supported', TagEnum.mime_media_type) : [b'application/pdf'],
 			(SectionEnum.printer, b'printer-is-accepting-jobs', TagEnum.boolean) : [parsers.Boolean(True).bytes()],
-			(SectionEnum.printer, b'queued-job-count', TagEnum.integer) : [parsers.Integer(0).bytes()],
+			(SectionEnum.printer, b'queued-job-count', TagEnum.integer) : [b'\x00'],
 			(SectionEnum.printer, b'pdl-override-supported', TagEnum.keyword) : [b'not-attempted'],
 			(SectionEnum.printer, b'printer-up-time', TagEnum.integer) : [parsers.Integer(self.printer_uptime()).bytes()],
 			(SectionEnum.printer, b'compression-supported', TagEnum.keyword) : [b'none'],
@@ -206,24 +206,24 @@ class StatelessPrinter(Behaviour):
 
 	def print_job_attributes(self, job_id, state, state_reasons):
 		# state reasons come from rfc2911 section 4.3.8
-		job_uri = b'%sjob/%s' % (self.base_uri, parsers.Integer(job_id).bytes(),)
+		job_uri = b'%sjob/%i' % (self.base_uri, job_id)
 
 		attr = {
 			# Required for print-job:
 
 			(SectionEnum.operation, b'job-uri', TagEnum.uri): [job_uri],
-			(SectionEnum.operation, b'job-id', TagEnum.integer): [parsers.Integer(int(job_id)).bytes()],
+			(SectionEnum.operation, b'job-id', TagEnum.integer): [parsers.Integer(job_id).bytes()],
 			(SectionEnum.operation, b'job-state', TagEnum.enum): [parsers.Enum(state).bytes()],
 			(SectionEnum.operation, b'job-state-reasons', TagEnum.keyword): state_reasons,
 
 			# Required for get-job-attributes:
 
 			(SectionEnum.operation, b'job-printer-uri', TagEnum.uri): [self.printer_uri],
-			(SectionEnum.operation, b'job-name', TagEnum.name_without_language) : [b'Print job %s' % parsers.Integer(job_id).bytes()],
+			(SectionEnum.operation, b'job-name', TagEnum.name_without_language) : [b'Print job %i' % job_id],
 			(SectionEnum.operation, b'job-originating-user-name', TagEnum.name_without_language) : [b'job-originating-user-name'],
-			(SectionEnum.operation, b'time-at-creation', TagEnum.integer) : [parsers.Integer(int(0)).bytes()],
-			(SectionEnum.operation, b'time-at-processing', TagEnum.integer) : [parsers.Integer(int(0)).bytes()],
-			(SectionEnum.operation, b'time-at-completed', TagEnum.integer) : [parsers.Integer(int(0)).bytes()],
+			(SectionEnum.operation, b'time-at-creation', TagEnum.integer) : [b'\x00'],
+			(SectionEnum.operation, b'time-at-processing', TagEnum.integer) : [b'\x00'],
+			(SectionEnum.operation, b'time-at-completed', TagEnum.integer) : [b'\x00'],
 			(SectionEnum.operation, b'job-printer-up-time', TagEnum.integer) : [parsers.Integer(self.printer_uptime()).bytes()],
 
 		}

commit c1fe21dd37d380d1536e5912386fdf029a9714b2

fix bug introduced with: 4003b8ba8bf9e1537c6c5c850eab351bb442670a, check integer tags

ippserver/behaviour.py
@@ -171,9 +171,8 @@ class StatelessPrinter(Behaviour):
         # Should have all these attributes:
         # https://tools.ietf.org/html/rfc2911#section-4.3
 
-        job_id = get_job_id(req)
         attributes = self.print_job_attributes(
-            job_id,
+            get_job_id(req),
             JobStateEnum.completed,
             [b'none']
         )
@@ -308,7 +307,7 @@ class StatelessPrinter(Behaviour):
                 SectionEnum.printer,
                 b'queued-job-count',
                 TagEnum.integer
-            ): [b'\x00'],
+            ): [b'\x00\x00\x00\x00'],
             (
                 SectionEnum.printer,
                 b'pdl-override-supported',
@@ -330,7 +329,7 @@ class StatelessPrinter(Behaviour):
 
     def print_job_attributes(self, job_id, state, state_reasons):
         # state reasons come from rfc2911 section 4.3.8
-        job_uri = b'%sjob/%s' % (self.base_uri, Integer(job_id))
+        job_uri = b'%sjob/%s' % (self.base_uri, Integer(job_id).bytes())
         attr = {
             # Required for print-job:
             (
@@ -375,17 +374,17 @@ class StatelessPrinter(Behaviour):
                 SectionEnum.operation,
                 b'time-at-creation',
                 TagEnum.integer
-            ): [b'\x00'],
+            ): [b'\x00\x00\x00\x00'],
             (
                 SectionEnum.operation,
                 b'time-at-processing',
                 TagEnum.integer
-            ): [b'\x00'],
+            ): [b'\x00\x00\x00\x00'],
             (
                 SectionEnum.operation,
                 b'time-at-completed',
                 TagEnum.integer
-            ): [b'\x00'],
+            ): [b'\x00\x00\x00\x00'],
             (
                 SectionEnum.operation,
                 b'job-printer-up-time',
@@ -404,8 +403,7 @@ class StatelessPrinter(Behaviour):
         The StatelessPrinter does not care about the id, but perhaps
         it can be subclassed into something that keeps track of jobs.
         """
-        job_id = random.randint(1,9999)
-        return job_id
+        return random.randint(1,9999)
 
     def handle_postscript(self, ipp_request, postscript_file):
         raise NotImplementedError

ippserver/request.py
@@ -8,7 +8,7 @@ import operator
 import itertools
 
 from .parsers import read_struct, write_struct
-from .constants import SectionEnum
+from .constants import SectionEnum, TagEnum
 
 
 class IppRequest(object):
@@ -80,6 +80,7 @@ class IppRequest(object):
         version_major, version_minor = 1, 1
         write_struct(f, b'>bb', version_major, version_minor)
         write_struct(f, b'>hi', self.opid_or_status, self.request_id)
+
         for section, attrs_in_section in itertools.groupby(
             sorted(self._attributes.keys()), operator.itemgetter(0)
         ):
@@ -93,6 +94,8 @@ class IppRequest(object):
                         f.write(name)
                     else:
                         write_struct(f, b'>h', 0)
+                    # Integer must be 4 bytes
+                    assert (tag != TagEnum.integer or len(value) == 4)
                     write_struct(f, b'>h', len(value))
                     f.write(value)
         write_struct(f, b'>B', SectionEnum.END)